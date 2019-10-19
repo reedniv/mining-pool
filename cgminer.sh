21c72dee3358e9e7c6638e6e431da0ec064d114e
reedniv <26852596+reedniv@users.noreply.github.com>
2019-10-19 07:03:00 +0700

    Create cgminer.sh

---
+++ cgminer.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+
+try="y"
+while [ $try = "y" ]
+do
+  clear
+	echo "\033[1;31m __  __ _       _               ____             _"
+		echo "|  \/  (_)_ __ (_)_ __   __ _  |  _ \ ___   ___ | |"
+		echo "| |\/| | | '_ \| | '_ \ / _' | | |_) / _ \ / _ \| |"
+		echo "| |  | | | | | | | | | | (_| | |  __/ (_) | (_) | |"
+		echo "|_|  |_|_|_| |_|_|_| |_|\__, | |_|   \___/ \___/|_|"
+		echo "                         |___/"
+		echo "            \033[1;32m  CGminer Multi Installer\n\n"
+		echo "\033[1;32m"
+		echo -n "Do Tou Want To Continue (Y/N) : "
+  read select;
+  if [ $select = "y" ]||[ $select = "Y" ]
+  then
+      echo "Installing Starting....!"
+		apt-get update && apt-get upgrade -y
+		apt-get install git build-essential cmake libuv1-dev libmicrohttpd-dev libssl-dev -y
+		apt-get install -qqy automake
+		apt-get install -qqy libcurl4-openssl-dev
+		apt-get install -qqy make
+		git clone https://github.com/ckolivas/cgminer.git
+		cd cgminer
+		chmod +x ./autogen.sh
+		./autogen.sh
+		./configure CFLAGS="-O3"
+		make
+		echo "Installing Successful"
+		exit
+  elif [ $select = "n" ]||[ $select = "N" ]
+  then
+      echo "\033[1;31mBye Bye.....!"
+      sleep 2
+      exit
+  else
+     echo "\033[1;31mERROr : Wrong Input....!"
+     sleep 2
+  fi
+done
